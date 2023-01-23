1416d50f6a966058ac6c44b8405f822df0b2d1b6
HTThierry <thierry.huguet@viacesi.fr>
2023-01-23 17:35:58 +0100

    Add project files.

---
+++ sandbox/Program.cs
@@ -0,0 +1,29 @@
+﻿//calculator console app
+
+using System;
+
+Console.WriteLine("enter first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter operator");
+string op = Console.ReadLine();
+
+switch (op)
+{
+    case "+" or "add":
+        Console.WriteLine(num1 + num2);
+        break;
+    case "-":
+        Console.WriteLine(num1 - num2);
+        break;
+    case "*":
+        Console.WriteLine(num1 * num2);
+        break;
+    case "/":
+        Console.WriteLine(num1 / num2);
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+}

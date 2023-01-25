cdf89570e28d4388a8586b3b412694722369399c
HTThierry <thierry.huguet@viacesi.fr>
2023-01-25 17:55:50 +0100

    Added complete save function

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,29 +1,58 @@
-﻿//calculator console app
-
-using System;
-
-Console.WriteLine("enter first number");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("enter second number");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("enter operator");
-string op = Console.ReadLine();
-
-switch (op)
+﻿namespace sandbox
 {
-    case "+" or "add":
-        Console.WriteLine(num1 + num2);
-        break;
-    case "-":
-        Console.WriteLine(num1 - num2);
-        break;
-    case "*":
-        Console.WriteLine(num1 * num2);
-        break;
-    case "/":
-        Console.WriteLine(num1 / num2);
-        break;
-    default:
-        Console.WriteLine("invalid operator");
-        break;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            string path1;
+            string path2;
+
+            Console.WriteLine("type de sauvegarde :");
+            Console.WriteLine("1 - Sauvegarde Complète (default)");
+            Console.WriteLine("2 - Sauvegarde Incrementale");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("chemin de sauvegarde initial :");
+            path1 = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("chemin de sauvegarde final :");
+            path2 = Convert.ToString(Console.ReadLine());
+
+            Program p = new Program();
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Sauvegarde Complète");
+                    p.Complete(path1, path2);
+                    break;
+                case 2:
+                    Console.WriteLine("Sauvegarde Incrementale");
+                    break;
+                default:
+                    Console.WriteLine("Sauvegarde Complète");
+                    break;
+            }
+
+            
+        }
+
+        void Complete(string PathFrom, string PathTo)
+        {
+            Console.WriteLine("Vérification de l'existance du dossier..........");
+            if (System.IO.Directory.Exists(PathFrom))
+            {
+                //Now Create all of the directories
+                foreach (string dirPath in Directory.GetDirectories(PathFrom, "*", SearchOption.AllDirectories))
+                {
+                    Directory.CreateDirectory(dirPath.Replace(PathFrom, PathTo));
+                }
+                Console.WriteLine("Copie en cours..........");
+                foreach (string newPath in Directory.GetFiles(PathFrom, "*.*", SearchOption.AllDirectories))
+                {
+                    File.Copy(newPath, newPath.Replace(PathFrom, PathTo), true);
+                }
+            }
+        }
+    }
 }
